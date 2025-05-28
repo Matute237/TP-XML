067c2fdf31dbd78945ff6e3fe76cd63200e6003b
Matu <matias.e.lopez747@gmail.com>
2025-05-28 17:02:52 -0300

    a

--- scrips/import_especialidades.py
+++ scrips/import_especialidades.py
@@ -1,5 +1,8 @@
 import sys
 import os
+BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+if BASE_DIR not in sys.path:
+    sys.path.insert(0, BASE_DIR)
 from app import create_app, db
 from xml.etree import ElementTree as ET
 
