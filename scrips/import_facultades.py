3b53d42f20a4d481d29e339349921b51d4119583
Nico <nicolas.andino04@gmail.com>
2025-05-28 17:05:22 -0300

    arreglo en las primeras lineas

--- scrips/import_facultades.py
+++ scrips/import_facultades.py
@@ -1,4 +1,8 @@
 import os
+import sys
+BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+if BASE_DIR not in sys.path:
+    sys.path.insert(0, BASE_DIR)
 from app import create_app, db
 from xml.etree import ElementTree as ET
 
