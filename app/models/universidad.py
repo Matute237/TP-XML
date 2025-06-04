0d66209c365a20cd323332461dd32967ed557344
Matu <matias.e.lopez747@gmail.com>
2025-06-04 19:37:14 -0300

    agrego import universidad

--- app/models/universidad.py
+++ app/models/universidad.py
@@ -6,4 +6,4 @@ class Universidad(db.Model):
     __tablename__ = "universidad"
     id: int = db.Column(db.Integer, primary_key=True, autoincrement=True)
     nombre: str = db.Column(db.String(100), nullable=False)
-    sigla: str = db.Column(db.String(10), nullable=False)
+    sigla: str = db.Column(db.String(10), nullable=True)
